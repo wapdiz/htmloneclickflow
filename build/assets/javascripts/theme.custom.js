1ea3a9ab29e4227e60f99a48a80b0867dc4bd88d
Your Namegit config --global user.name wapdiz <oslife@mail.ru>
2015-09-12 02:49:45 +0700

    Потоки: просмотр

--- build/assets/javascripts/theme.custom.js
+++ build/assets/javascripts/theme.custom.js
@@ -1,1 +1,4 @@
 /* Add here all your JS customizations */
+
+var elem = document.querySelector('.js-switch');
+var switchery = new Switchery(elem, { disabled: true });
--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ var gulp = require('gulp'),
     imagemin = require('gulp-imagemin'),
     pngquant = require('imagemin-pngquant'),
     rimraf = require('rimraf'),
+    fileinclude = require('gulp-file-include'),
     browserSync = require("browser-sync"),
     stylus = require('gulp-stylus'),
     spritesmith = require('gulp.spritesmith'),
@@ -23,9 +24,9 @@ var path = {
         img: 'build/images/'
     },
     src: { //Пути откуда брать исходники
-        html: 'src/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
+        html: 'src/*.html', 
         style: 'src/assets/stylesheets/*.scss',
-        img: 'src/images/**/*.*' //Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
+        img: 'src/images/**/*.*'
     },
     watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
         html: 'src/**/*.html',
@@ -47,8 +48,12 @@ var config = {
 
 gulp.task('html:build', function () {
     gulp.src(path.src.html) //Выберем файлы по нужному пути
-        .pipe(rigger()) //Прогоним через rigger
-        .pipe(gulp.dest(path.build.html)) //Выплюнем их в папку build
+        .pipe(rigger())
+        .pipe(fileinclude({
+          prefix: '@@',
+          basepath: '@file'
+        }))
+        .pipe(gulp.dest(path.build.html)) //И сохраним в папку build
         .pipe(reload({stream: true})); //И перезагрузим наш сервер для обновлений
 });
 
@@ -59,20 +64,20 @@ gulp.task('style:build', function () {
         .pipe(sass({errLogToConsole: true}))
         // .pipe(cssmin({compatibility:'ie8'})) //Сожмем
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest(path.build.css)) //И в build
+        .pipe(gulp.dest(path.build.css))
         .pipe(reload({stream: true}));
 });
 
 gulp.task('image:build', function () {
-    gulp.src(path.src.img) //Выберем наши картинки
-        .pipe(imagemin({ //Сожмем их
+    gulp.src(path.src.img)
+        .pipe(imagemin({
             optimizationLevel: 7,
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             use: [pngquant()],
             interlaced: true
         }))
-        .pipe(gulp.dest(path.build.img)) //И бросим в build
+        .pipe(gulp.dest(path.build.img))
         .pipe(reload({stream: true}));
 });
 
@@ -96,10 +101,6 @@ gulp.task('watch', function(){
 
 gulp.task('webserver', function () {
     browserSync(config);
-});
-
-gulp.task('clean', function (cb) {
-    rimraf(path.clean, cb);
 });
 
 gulp.task('default', ['build', 'webserver', 'watch']);

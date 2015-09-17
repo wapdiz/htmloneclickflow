8c63015614e1cebd00ad1eb92300966a4fef3910
Your Namegit config --global user.name wapdiz <oslife@mail.ru>
2015-09-17 08:15:20 +0700

    Статистика + изменения

--- build/assets/javascripts/theme.custom.js
+++ build/assets/javascripts/theme.custom.js
@@ -3,32 +3,34 @@
 (function( $ ) {
 	$(function() {
 		$('.lp-listing>ul>li:nth-child(5n+4),.lp-listing>ul>li:nth-child(5n)').hover(function() {
-			// alert('hover');
 			// $(this).closest('.lp-listing').addClass('lp-listing-active');
 			$(this).closest('ul').css('left','-180px');
 		},
 		function() {
-			// alert('unhover');
 			// $(this).closest('.lp-listing').removeClass('lp-listing-active');
 			$(this).closest('ul').css('left','0');
 		});
 		
 
-		$( ".lp-listing1" ).mousemove(function( event ) {
-		  var pageCoords = "( " + event.pageX + ", " + event.pageY + " )";
-		  var clientCoords = "( " + event.clientX + ", " + event.clientY + " )";
-		  var p = $( ".lp-listing>ul>li:nth-child(4n)" );
-		  var offset = p.offset();
-		  if (event.pageX>=offset.left){
-		  	$('.lp-listing>ul').css('left','-180px');
-		  }
-		  else{
-		  	$('.lp-listing>ul').css('left','0');
-		  }
-		  $( ".frf" ).text( "( event.pageX, event.pageY ) : " + pageCoords );
+		// $( ".lp-listing1" ).mousemove(function( event ) {
+		//   var pageCoords = "( " + event.pageX + ", " + event.pageY + " )";
+		//   var clientCoords = "( " + event.clientX + ", " + event.clientY + " )";
+		//   var p = $( ".lp-listing>ul>li:nth-child(4n)" );
+		//   var offset = p.offset();
+		//   if (event.pageX>=offset.left){
+		//   	$('.lp-listing>ul').css('left','-180px');
+		//   }
+		//   else{
+		//   	$('.lp-listing>ul').css('left','0');
+		//   }
+		//   $( ".frf" ).text( "( event.pageX, event.pageY ) : " + pageCoords );
+		// });
+		$('.timebuttons button').click(function() {
+
+			$('.timebuttons li div').removeClass('timebuttons-pos-active');
+			$(this).parent('div').addClass('timebuttons-pos-active');
 		});
-
-
+	
 	});
 
 })( jQuery );

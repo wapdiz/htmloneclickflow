4e23963bc77afe8e07c6cb655786b2f0f43c01e7
Your Namegit config --global user.name wapdiz <oslife@mail.ru>
2015-10-28 03:22:17 +0700

    update js

--- build/assets/javascripts/theme.custom.js
+++ build/assets/javascripts/theme.custom.js
@@ -2,16 +2,6 @@
 
 (function( $ ) {
 	$(function() {
-		// if (window.matchMedia('(min-width: 770px)').matches) {
-		// 	$('.lp-listing>ul>li:nth-child(5n+4),.lp-listing>ul>li:nth-child(5n)').hover(function() {
-		// 		$(this).closest('ul').css('left','-170px');
-		// 	},
-		// 	function() {
-		// 		$(this).closest('ul').css('left','0');
-		// 	});
-		// 	$('.lp-listing>ul>li:nth-child(5n)').each( function () { $(this).parent().after( $(this).nextAll().wrapAll('<ul/>').parent() ) } );
-		// }
-
 		$('.timebuttons button').click(function() {
 			$('.timebuttons li div').removeClass('timebuttons-pos-active');
 			$(this).parent('div').addClass('timebuttons-pos-active');
